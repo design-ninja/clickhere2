ee6948cd6356ac1deb4af22d24b5feaff5cf8244
lirik <kirill.isachenko@gmail.com>
2023-05-02 13:20:04 +0700

    fix

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 // Scroll to Jobs section on header button click
 
-window.onload = function() {
+window.onload = function () {
     scrollTo = (element) => {
         window.scroll({
             left: 0,
@@ -18,7 +18,7 @@ window.onload = function() {
     document.getElementById("toServices").addEventListener('click', () => {
         scrollTo(document.getElementById("services"));
     });
-      
+
     document.getElementById("toJobs").addEventListener('click', () => {
         scrollTo(document.getElementById("jobs"));
     });
@@ -26,14 +26,4 @@ window.onload = function() {
     document.getElementById("toContact").addEventListener('click', () => {
         scrollTo(document.getElementById("contact"));
     });
-  }
-
-
-// Buttons events
-
-const name = 'cl-he';
-const domain = 'com';
-
-function action(text) {
-    window.location.href=`mailto:${text}@${name}.${domain}`;
 }
